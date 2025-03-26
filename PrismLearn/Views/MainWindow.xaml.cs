5cc48cfdc13ee94de2eb3d556007c61eef6c52b4
qiantao <qiantao@DESKTOP-JIPU9VE>
2025-03-26 15:17:10 +0800

    test: 测试prism
    进行prism测试
    
    Closes #123

---
+++ PrismLearn/Views/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace PrismLearn.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
